574b149d98eb13308cfd353fcce6d54d5ac2b9d2
Madhav Sharma <madhavsh746@gmail.com>
2026-04-15 14:59:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Mongo URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI, {
       serverSelectionTimeoutMS: 10000,
     });
+
     console.log("✅ Connected to MongoDB");
   } catch (error) {
-    console.error("❌ Full MongoDB Error:", error);
+    console.error("❌ Full MongoDB Error:", error.message);
     process.exit(1);
   }
 };
